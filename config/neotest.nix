b01b83f2306c055ae8bbe6ad692656b0bb3f4ff8
Alejandro Gomez <alejandro@meetiqm.com>
2025-01-09 13:24:56 +0200

    Make neotest not lazy

--- config/neotest.nix
+++ config/neotest.nix
@@ -8,20 +8,20 @@
   plugins.neotest = {
     enable = true;
     adapters.python.enable = true;
-    lazyLoad.enable = true;
+    # lazyLoad.enable = true;
     #
-    lazyLoad.settings = {
-      cmd = "Neotest";
+    # lazyLoad.settings = {
+      # cmd = "Neotest";
     #   __unkeyed-1 = "<leader>tr";
     #   __unkeyed-2 = "<leader>tt";
-    };
+    # };
   };
   keymaps = [
     {
       mode = "n";
       key = "<leader>tr";
-      action = ":Neotest run<CR>";
-      # action.__raw = ''function() require("neotest").run.run() end'';
+      # action = ":Neotest run<CR>";
+      action.__raw = ''function() require("neotest").run.run() end'';
       options.desc = "Test Nearest";
     }
     {
