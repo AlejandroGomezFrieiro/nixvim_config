5de637b56153bddfb615352255ac8bd6c21d2382
Alejandro <alejandrogomezfrieiro@gmail.com>
2025-06-07 00:15:44 +0300

    Adds openrouter to codecompanion

--- config/codecompanion.nix
+++ config/codecompanion.nix
@@ -8,7 +8,33 @@
     enable = true;
 
     settings = {
+      strategies = {
+        agent = { adapter = "openrouter_claude";};
+        chat = { adapter = "openrouter_claude";};
+        inline = { adapter = "openrouter_claude";};
+      };
+
       adapters = {
+        openrouter_claude = {
+          __raw = ''
+            function()
+              return require("codecompanion.adapters").extend("openai_compatible", {
+                env = {
+                  url="https://openrouter.ai/api";
+                  api_key=os.getenv("OPENROUTER_API_KEY");
+                  chat_url = "/v1/chat/completions",
+                };
+                schema = {
+                  model = {
+                    default = "deepseek/deepseek-r1-0528-qwen3-8b:free",
+
+                  };
+
+                };
+              })
+            end
+          '';
+        };
         ollama = {
           __raw = ''
             function()
